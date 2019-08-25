2cd04a304f20c467a6973c5b8a7210b346bd2c8e
iamrazak <54507341+iamrazak@users.noreply.github.com>
2019-08-25 17:51:51 +0100

    Add files via upload

---
+++ Haruna Abdulrazak 1019.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 15:42:59 2019
+
+@author: ALEDARE
+"""
+#U18/FNS/CSC/1019
+#HARUNA ABDULRAZAK ENEGAMHE
+import math
+x = int(input("Enter x Number:"))
+y  = int(input("Enter y Number:"))
+answer = x**y
+print (answer)
+print ("x raised to the power of y is:",answer)
+print (math.log(x,2))
+print (math.log2(x))
